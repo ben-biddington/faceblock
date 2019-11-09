904ee5630029cf70f8bd5fd5e4bf61c921e9462f
ben.biddington <ben.biddington@gmail.com>
2019-11-10 10:32:48 +1300

    This is hard work

--- adapters/web/popup/info.js
+++ adapters/web/popup/info.js
@@ -1,21 +1,65 @@
 function get() 
 {
-  return browser.storage.local.get().then(results => results.names || []);
+  return browser.storage.local.get().
+  then(results => results.names || []).
+  then(result => {
+    status(`Returning: ${result.length}`);  
+    return result;
+  });
+}
+
+function set(newNames = []) 
+{
+  var next = { names: newNames };
+
+  status(next);
+
+  return browser.storage.local.set(next);
 }
 
 function status(m) {
-  document.querySelector("#log").innerHTML = m;
+  document.querySelector("#log").innerText = m;
 }
 
 function addNew() {
   var newName = document.querySelector('#n').value || 'abc';
-
+  
   return get().
-    then(existingNames => {
-      console.log(`Existing: ${existingNames}`);
-      return browser.storage.local.set({ names: [...existingNames, newName] });
-    }).
-    then(() => status(`Added <${newName}>`));
+    then((existingNames) => set([ ...existingNames, newName ])).
+    then(()              => showCurrent());
+}
+
+function remove(name) {
+  return get().
+    then(existingNames => set(existingNames.filter(it => it != name))).
+    then(() => showCurrent());
+}
+
+function showCurrent() {
+  var settings = document.querySelector('#settings');
+
+  var list = document.createElement('ul');
+
+  return get().then(results => {
+    status(`showing <${results.length}> results`);
+    results.forEach(r => {
+      var deleteButton = document.createElement('button');
+      deleteButton.innerText = 'x';
+      deleteButton.addEventListener('click', remove(r));
+      
+      var text = document.createElement('span');
+      text.setAttribute('style', 'margin-left: 2px');
+      text.innerText = escape(r);
+
+      var li = document.createElement('li');
+      li.appendChild(deleteButton);
+      li.appendChild(text);
+      
+      list.appendChild(li);
+    });
+
+    settings.appendChild(list);
+  });
 }
 
 var addButton = document.querySelector('#add');
@@ -24,11 +68,7 @@ addButton.addEventListener('click', addNew);
 try 
 {
   // [i] https://github.com/mdn/webextensions-examples/blob/master/navigation-stats/popup.js
-  get().
-    then(results => results.names || []).
-    then(results => {
-      status(`results: ${JSON.stringify(results)}`);
-    });
+  showCurrent();
 }
 catch (e)
 {
